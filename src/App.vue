4b6e74b7f0af63182309de2c5e76d061c039d180
iqbaldandy <iqbalkawag@gmail.com>
2020-07-08 20:38:15 +0700

    Second Commit

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,10 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
+
+  
+    
+  
 </template>
 
 <script>
@@ -12,6 +16,22 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data: {
+    items:[],
+   item:['NEW VALUE','NEW VALUE_2'],
+   
+  },
+  created(){
+  	this.items.push(this.item);
+  },
+  methods: 
+  {
+    add()
+    {
+      this.item = ['</br><input type="text"/>','<input type="text"/></br>'];
+      this.items.push(this.item);
+    }
   }
 }
 </script>
