03ca0cecdf0b6ffc16f46c0fc1fcc1f48d219d0d
iqbaldandy <iqbalkawag@gmail.com>
2020-07-23 03:02:28 +0700

    Calendar

--- src/Desktop5.vue
+++ src/Desktop5.vue
@@ -214,7 +214,65 @@
                       </b-form>
                   </b-tab>
                   <b-tab title="SCHEDULE">
-                    <p>I'm the second tab</p>
+                    <v-app id="dayspan" v-cloak>
+                     <ds-calendar-app ref="app"
+                        :calendar="calendar"
+                        :read-only="readOnly"
+                        @change="saveState">
+
+                        <!-- <template slot="title">
+                          DaySpan
+                        </template> -->
+
+                        <!-- <template slot="menuRight">
+                          <v-btn icon large href="https://github.com/ClickerMonkey/dayspan-vuetify" target="_blank">
+                            <v-avatar size="32px" tile>
+                              <img src="https://simpleicons.org/icons/github.svg" alt="Github">
+                            </v-avatar>
+                          </v-btn>
+                        </template> -->
+
+                        <template slot="eventPopover" slot-scope="slotData">
+                          <ds-calendar-event-popover
+                            v-bind="slotData"
+                            :read-only="readOnly"
+                            @finish="saveState"
+                          ></ds-calendar-event-popover>
+                        </template>
+
+                        <template slot="eventCreatePopover" slot-scope="{placeholder, calendar}">
+                          <ds-calendar-event-create-popover
+                            :calendar-event="placeholder"
+                            :calendar="calendar"
+                            :close="$refs.app.$refs.calendar.clearPlaceholder"
+                            @create-edit="$refs.app.editPlaceholder"
+                            @create-popover-closed="saveState"
+                          ></ds-calendar-event-create-popover>
+                        </template>
+
+                        <template slot="eventTimeTitle" slot-scope="{calendarEvent, details}">
+                          <div>
+                            <v-icon class="ds-ev-icon"
+                              v-if="details.icon"
+                              size="14"
+                              :style="{color: details.forecolor}">
+                              {{ details.icon }}
+                            </v-icon>
+                            <strong class="ds-ev-title">{{ details.title }}</strong>
+                          </div>
+                          <div class="ds-ev-description">{{ getCalendarTime( calendarEvent ) }}</div>
+                        </template>
+
+                        <template slot="drawerBottom">
+                          <div class="pa-3">
+                            <v-checkbox
+                              label="Read Only?"
+                              v-model="readOnly"
+                            ></v-checkbox>
+                          </div>
+                        </template>
+                      </ds-calendar-app>
+                    </v-app>
                   </b-tab>
               </b-tabs>
               </div>
@@ -228,6 +286,8 @@
 </template>
 
 <script>
+import { Calendar, Month } from 'dayspan';
+import Vue from 'vue';
 export default {
   name: 'app',
   data () {
@@ -250,8 +310,30 @@ export default {
         cpassword: '',
         username: 'RikaJOG',
         attribute1: 'Attribute',   
+      },
+      storeKey: 'dayspanState',
+      calendar: Calendar.months(),
+      readOnly: false,
+      defaultEvents: [
+      {
+        data: {
+          title: 'Christmas Day',
+          color: '#2196F3',
+          calendar: 'US Holidays'
+        },
+        schedule: {
+          month: [Month.DECEMBER],
+          dayOfMonth: [25]
+        }
       }
+    ]
     }
+  },
+   mounted()
+  {
+    window.app = this.$refs.app;
+
+    this.loadState();
   },
   methods: {
       greet: function () {
@@ -285,11 +367,73 @@ export default {
           this.$nextTick(() => {
             this.show = true
           })
+        },
+        getCalendarTime(calendarEvent)
+        {
+          let sa = calendarEvent.start.format('a');
+          let ea = calendarEvent.end.format('a');
+          let sh = calendarEvent.start.format('h');
+          let eh = calendarEvent.end.format('h');
+
+          if (calendarEvent.start.minute !== 0)
+          {
+            sh += calendarEvent.start.format(':mm');
+          }
+
+          if (calendarEvent.end.minute !== 0)
+          {
+            eh += calendarEvent.end.format(':mm');
+          }
+
+          return (sa === ea) ? (sh + ' - ' + eh + ea) : (sh + sa + ' - ' + eh + ea);
+        },
+
+        saveState()
+        {
+          let state = this.calendar.toInput(true);
+          let json = JSON.stringify(state);
+
+          localStorage.setItem(this.storeKey, json);
+        },
+
+        loadState()
+        {
+          let state = {};
+
+          try
+          {
+            let savedState = JSON.parse(localStorage.getItem(this.storeKey));
+
+            if (savedState)
+            {
+              state = savedState;
+              state.preferToday = false;
+            }
+          }
+          catch (e)
+          {
+            // eslint-disable-next-line
+            console.log( e );
+          }
+
+          if (!state.events || !state.events.length)
+          {
+            state.events = this.defaultEvents;
+          }
+
+          state.events.forEach(ev =>
+          {
+            let defaults = this.$dayspan.getDefaultEventDetails();
+
+            ev.data = Vue.util.extend( defaults, ev.data );
+          });
+
+          this.$refs.app.setState( state );
         }
+      }
       // addRowInput() {
       //   this.item = ['<p>test<p>'];
       //   this.items.push(this.item);
       // }
     }
-}
 </script>
--- src/main.js
+++ src/main.js
@@ -1,17 +1,32 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify'
+import DaySpanVuetify from 'dayspan-vuetify'
 // import App from './App.vue'
-import Desktop1 from './Desktop1.vue'
+// import Desktop1 from './Desktop1.vue'
 // import AnimSidebar from './AnimSidebar.vue'
-// import Desktop5 from './Desktop5.vue'
+// import DaySpan from './DaySpan.vue'
+import Desktop5 from './Desktop5.vue'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import 'vuetify/dist/vuetify.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import 'dayspan-vuetify/dist/lib/dayspan-vuetify.min.css'
+
 import './style/style.css'
 
+Vue.use(Vuetify)
 Vue.use(BootstrapVue)
+
+Vue.use(DaySpanVuetify, {
+  methods: {
+    getDefaultEventColor: () => '#1976d2'
+  }
+});
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(Desktop1),
+  render: h => h(Desktop5),
 }).$mount('#app')
